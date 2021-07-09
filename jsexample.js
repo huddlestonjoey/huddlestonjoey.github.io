1cbaeb4663cea4a89ab856b480277c3e2cc737cc
joey <mindyh1108@aol.com>
2021-07-09 13:42:12 -0600

    initial commit

---
+++ jsexample.js
@@ -0,0 +1,6 @@
+function changeClass(){
+    document.getElementById("changeClass").className = "purple";
+}
+function changeText(){
+    document.getElementById("changeText").innerHTML = "Text was changed";
+}
